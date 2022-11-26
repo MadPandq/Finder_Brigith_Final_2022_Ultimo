b5643a51bb769452137b5f5c0d6ba63c49358ff9
Brigith <brigith.a.r.q@gmail.com>
2022-11-26 03:31:30 -0300

    version 2.0

--- app/src/main/java/com/example/finder/Login.java
+++ app/src/main/java/com/example/finder/Login.java
@@ -1,36 +1,97 @@
 package com.example.finder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-    TextView text;
-    Button boton;
+import java.util.regex.Pattern;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    TextView textreg;
+    Button botonLogin;
+
+    private EditText contraseña, email;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        text= findViewById(R.id.tv_register);
-        boton= findViewById(R.id.btnlogin);
+        textreg = (TextView) findViewById(R.id.tv_register);
+        textreg.setOnClickListener(this);
 
-        //hacemos uso del setOnclick
-        boton.setOnClickListener(view -> {
-            Intent intent = new Intent(Login.this,MainActivity.class);
-            startActivity(intent);
+        botonLogin = findViewById(R.id.btnlogin);
+        botonLogin.setOnClickListener((View.OnClickListener) this);
 
-        });
+        email = (EditText) findViewById(R.id.email);
+        contraseña = (EditText) findViewById(R.id.contraseña);
 
-        text.setOnClickListener(view -> {
-            Intent i = new Intent(Login.this, Registrar.class);
-            startActivity(i);
-        });
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_register:
+                startActivity(new Intent(this, Registrar.class));
+                break;
+
+            case R.id.btnlogin:
+                userLogin();
+                break;
+        }
+    }
+
+    private void userLogin() {
+        String emmail = email.getText().toString().trim();
+        String contra = contraseña.getText().toString().trim();
+
+        if (emmail.isEmpty()) {
+            email.setError("Es necesario ingresar un email");
+            email.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(emmail).matches()){
+            email.setError("Ingresa un email por favor");
+            email.requestFocus();
+            return;
+        }
+
+        if (contra.isEmpty()) {
+            contraseña.setError("Es necesario ingresar una contraseña");
+            contraseña.requestFocus();
+            return;
+        }
+
+       mAuth.signInWithEmailAndPassword(emmail, contra).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+           @Override
+           public void onComplete(@NonNull Task<AuthResult> task) {
+               if(task.isSuccessful()){
+
+                   startActivity(new Intent(Login.this, MainActivity.class));
+               }
+               else{
+                   Toast.makeText(Login.this, "Falló el login", Toast.LENGTH_SHORT).show();
+               }
+           }
+       });
+    }
 }
--- app/src/main/java/com/example/finder/Registrar.java
+++ app/src/main/java/com/example/finder/Registrar.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -16,11 +18,13 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Registrar extends AppCompatActivity {
+public class Registrar extends AppCompatActivity implements View.OnClickListener {
+
 
     private EditText nombreRegis, apellidoRegis, rutRegis, contraRegis, correoRegis, direcRegis;
     private FirebaseAuth mAuth;
     private Spinner spinner_genero;
+    private Button validar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,78 +43,86 @@ public class Registrar extends AppCompatActivity {
         contraRegis = (EditText) findViewById(R.id.contraRegistro);
         correoRegis = (EditText) findViewById(R.id.correoRegistro);
         direcRegis = (EditText) findViewById(R.id.direccionRegistro);
+        validar = (Button) findViewById(R.id.btnValidar);
+        validar.setOnClickListener(this);
 
     }
 
-    private void registerUser(){
-        String nombre = nombreRegis.getText().toString().trim();
-        String apellido = apellidoRegis.getText().toString().trim();
-        String direccion = direcRegis.getText().toString().trim();
-        String correo = correoRegis.getText().toString().trim();
-        String contraseña = contraRegis.getText().toString().trim();
-        String rut = rutRegis.getText().toString().trim();
-
-        if(nombre.isEmpty()){
-            nombreRegis.setError("Es necesario ingresar un nombre");
-            nombreRegis.requestFocus();
-            return;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnValidar:
+                registrarUsuario();
+                break;
         }
+    }
 
-        if(apellido.isEmpty()){
-            apellidoRegis.setError("Es necesario ingresar un apellido");
-            apellidoRegis.requestFocus();
-            return;
-        }
+    private void registrarUsuario() {
+            String nombre = nombreRegis.getText().toString().trim();
+            String apellido = apellidoRegis.getText().toString().trim();
+            String direccion = direcRegis.getText().toString().trim();
+            String correo = correoRegis.getText().toString().trim();
+            String contraseña = contraRegis.getText().toString().trim();
+            String rut = rutRegis.getText().toString().trim();
 
-        if(direccion.isEmpty()){
-            direcRegis.setError("Es necesario ingresar una dirección");
-            direcRegis.requestFocus();
-            return;
-        }
+            if(nombre.isEmpty()){
+                nombreRegis.setError("Es necesario ingresar un nombre");
+                nombreRegis.requestFocus();
+                return;
+            }
 
-        if(correo.isEmpty()){
-            correoRegis.setError("Es necesario ingresar un correo");
-            correoRegis.requestFocus();
-            return;
-        }
+            if(apellido.isEmpty()){
+                apellidoRegis.setError("Es necesario ingresar un apellido");
+                apellidoRegis.requestFocus();
+                return;
+            }
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
-            correoRegis.setError("Ingresa un correo válido");
-            correoRegis.requestFocus();
-            return;
-        }
+            if(direccion.isEmpty()){
+                direcRegis.setError("Es necesario ingresar una dirección");
+                direcRegis.requestFocus();
+                return;
+            }
 
-        if(contraseña.isEmpty()){
-            contraRegis.setError("Es necesario ingresar una contraseña");
-            contraRegis.requestFocus();
-            return;
-        }
+            if(correo.isEmpty()){
+                correoRegis.setError("Es necesario ingresar un correo");
+                correoRegis.requestFocus();
+                return;
+            }
 
-        if(rut.isEmpty()){
-            rutRegis.setError("Es necesario ingresar un rut");
-            rutRegis.requestFocus();
-            return;
-        }
+            if(!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+                correoRegis.setError("Ingresa un correo válido");
+                correoRegis.requestFocus();
+                return;
+            }
 
-        mAuth.createUserWithEmailAndPassword(correo, contraseña)
-                .addOnCompleteListener(task -> {
-                    if(task.isSuccessful()){
-                        Usuario usuario = new Usuario(nombre, apellido, rut, correo, contraseña,direccion);
+            if(contraseña.isEmpty()){
+                contraRegis.setError("Es necesario ingresar una contraseña");
+                contraRegis.requestFocus();
+                return;
+            }
 
-                        FirebaseDatabase.getInstance().getReference("Usuarios").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                .setValue(usuario).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-                                        if(task.isSuccessful()){
-                                            Toast.makeText(Registrar.this, "Usuario ha sido registrado", Toast.LENGTH_SHORT).show();
+            if(rut.isEmpty()){
+                rutRegis.setError("Es necesario ingresar un rut");
+                rutRegis.requestFocus();
+                return;
+            }
+
+            mAuth.createUserWithEmailAndPassword(correo, contraseña)
+                    .addOnCompleteListener(task -> {
+                        if(task.isSuccessful()){
+                            Usuario usuario = new Usuario(nombre, apellido, rut, correo, contraseña,direccion);
+
+                            FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(usuario).addOnCompleteListener(task1 -> {
+                                        if(task1.isSuccessful()){
+                                            Toast.makeText(Registrar.this, "Usuario ha sido registrado", Toast.LENGTH_LONG).show();
                                         }
 
                                         else{
-                                            Toast.makeText(Registrar.this, "Falló el registro", Toast.LENGTH_SHORT).show();
+                                            Toast.makeText(Registrar.this, "Falló el registro", Toast.LENGTH_LONG).show();
                                         }
-                                    }
-                                });
-                    }
-                });
+                                    });
+                        }
+                    });
+        }
     }
-}
